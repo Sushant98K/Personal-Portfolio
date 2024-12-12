f8dfeaaf896b5462bc07d017f9bf47ba3457c471
Sushant98K <sushant98k@gmail.com>
2024-12-12 18:31:28 +0530

    Content Changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -44,7 +44,7 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["HTML", "CSS", "React", "Pyhton", "DJango"],
   },
   {
     title: "Task Management App",
